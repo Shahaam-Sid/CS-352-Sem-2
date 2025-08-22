01fb7b31ad3234a8cc637d6aaa4484bf0456fdd1
Shahaam-Sid <muhammadgc821@gmail.com>
2025-08-22 23:11:55 +0500

    Added Class File and UML

---
+++ vectornd.py
@@ -0,0 +1,68 @@
+class NdVector:
+    def __init__(self, d):
+        self._coords = [0] * d
+        
+    def __len__(self):
+        """Returns Dimension of the Vector"""
+        
+        return len(self._coords)
+    
+    def __getitem__(self, j):
+        """Returns the Value of the given Axis"""
+        
+        return self._coords[j]
+    
+    def __setitem__(self, j, value):
+        """Set the value of the given Axis"""
+        
+        self._coords[j] = value
+        
+    def __add__(self, other):
+        """Addition of two Vectors"""
+        if len(self) != len(other):
+            raise ValueError("Dimensions must be same")
+        
+        result = NdVector(len(self))
+        for i in range(len(self)):
+            result[i] = self[i] + other[i]
+        
+        return result    
+    def __sub__(self, other):
+        """Subtraction of two Vectors"""
+        
+        if len(self) != len(other):
+            raise ValueError("Dimensions must be same")
+            
+        result = NdVector((len(self)))
+        for i in range(len(self)):
+            result[i] = self[i] - other[i]
+            
+        return result
+    
+    def __mul__(self, scalar):
+        """Multiplication of the Vector with a Scalar  Value"""
+        if isinstance(scalar, (NdVector, list)):
+            raise TypeError("Must be mulitplied by a Scalar Number")
+          
+        result = NdVector(len(self))
+        for i in range(len(self)):
+            result[i] = self[i] * scalar
+            
+        return result
+        
+    def __abs__(self):
+        """Returns the Magnitude of the Vector"""
+        result = 0
+        for i in range(len(self)):
+            result += self[i] ** 2
+        
+        return result ** 0.5
+        
+    def __str__(self):
+        """Returns a user-friendly string representation of the vector."""
+        return f"<{str(self._coords)[1:-1]}>"
+
+    
+    def __repr__(self):
+        """Returns the official string representation of the vector."""
+        return f"NdVector({self._coords!r})"
