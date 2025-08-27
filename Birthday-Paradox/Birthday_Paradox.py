61539fa7d4ef1ace4919a2fc9c386aa42500ee77
Shahaam-Sid <muhammadgc821@gmail.com>
2025-08-27 08:04:44 +0500

    Added Random Date Generator

--- Birthday-Paradox/Birthday_Paradox.py
+++ Birthday-Paradox/Birthday_Paradox.py
@@ -1,5 +1,4 @@
-import re
-from signal import raise_signal
+import random
 
 
 class BirthdayParadox:
@@ -9,10 +8,39 @@ class BirthdayParadox:
         n is the Number of people in test sample
         n must be more 23
         """
+        if not isinstance(n, int):
+            raise TypeError("Input must be an Integer")
         if n <= 23:
             raise ValueError("Number of People must be more 23")
         
         self._n = n
+        self.sample_list = []
+        
+        self.sample = []
+        self.dd = 0
+        self.mm = 0
+        
+        for i in range(0, self._n):
+            
+            self.sample = []
+            self.dd = 0
+            self.mm = 0
+            
+            self.mm = random.randint(1, 12)
+            if self.mm == 2:
+                self.dd = random.randint(1, 29)
+            elif self.mm in [4, 6, 9, 11]:
+                self.dd = random.randint(1, 30)
+            else:
+                self.dd = random.randint(1, 31)
+                
+            self.sample = [self.dd, self.mm]
+            self.sample_list.append(self.sample)
+        
+        
+    
+    def test(self):
+        print(self.sample_list)
         
     def __str__(self):
         return f"Birthday Paradox test for a sample of {self._n} pupils"
--- Birthday-Paradox/main.py
+++ Birthday-Paradox/main.py
@@ -4,8 +4,11 @@ try:
     x = BirthdayParadox(25)
     print(x)
     print(repr(x))
+    x.test()
     
     
+except TypeError as e:
+    print("Error:", e)
 except ValueError as e:
     print("Error:", e)
 except Exception:
