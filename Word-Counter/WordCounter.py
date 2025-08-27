fa516606dcda195be60a549c77995079ec05f413
Shahaam-Sid <muhammadgc821@gmail.com>
2025-08-27 22:31:13 +0500

    tweaked __repr__

--- Word-Counter/WordCounter.py
+++ Word-Counter/WordCounter.py
@@ -12,21 +12,32 @@ class WordCounter:
             raise ValueError("String must contain Words only")
         
         self._list_words = list_words
-        self.counts = {}
+        self._counts = {}
         
     
     def count_word(self):
         for word in self._list_words:
-            self.counts[word] = self.counts.get(word, 0) + 1
+            self._counts[word] = self._counts.get(word, 0) + 1
+            
+    def show_count(self):
+        for word, count in self._counts.items():
+            print(f"'{word}' is present {count} times")
         
     def __str__(self):
         return f"Sentence: {self._sentence}"
     
     def __repr__(self):
-        return f"WordCounter('Sentence': '{self._sentence}')"
+        return f"""
+WordCounter('sentence': '{self._sentence}')
+           'list_words': '{self._list_words}'
+           'self._counts: '{self._counts}'
+           
+"""
     
     
-x = WordCounter("123 is playing")
+x = WordCounter("Shahaam is playing")
+x.count_word()
+x.show_count()
 
 print(x)
 print(repr(x))
