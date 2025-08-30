637e2106118a1a8b64482c988ab7c6636dbcb844
Shahaam-Sid <muhammadgc821@gmail.com>
2025-08-30 10:08:44 +0500

    Added distance, __add__, __sub__ and __str__ functions

--- geometry/Point.py
+++ geometry/Point.py
@@ -30,3 +30,25 @@ class Point:
             raise TypeError("y must be a Integer or Decimal")
         
         self._y = value
+        
+    def distance(self, other):
+        if not isinstance(other, Point):
+            raise TypeError("Both inputs must be a Point")
+        
+        lenght = ((self.x - other.x) ** 2 + (self.y - other.y) ** 2) ** 0.5
+        
+        return f"Distance b/w {self} and {other} is {lenght}"
+    
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+            
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
+    
+    def __str__(self):
+        return f"Point: (x, y) = ({self.x}, {self.y})"
+
+p1 = Point(3, 4)
+p2 = Point(0, 0)
+# Distance between two points
+print(p1.distance(p2))  # 5.0  (classic 3-4-5 triangle)
