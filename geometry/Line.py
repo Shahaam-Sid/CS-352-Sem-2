a212781f6846afab8b74f39b10d7fa2d75d646a5
Shahaam-Sid <muhammadgc821@gmail.com>
2025-08-30 10:59:48 +0500

    Added Docstrings to Line.py

--- geometry/Line.py
+++ geometry/Line.py
@@ -1,7 +1,18 @@
-from geometry.Point import Point
+from Point import Point
 
 class Line:
+    """
+    Creates Lines from Two Point, from Point Class
+    """
     def __init__(self, start_point, end_point):
+        """
+        Takes start_point and end_point as Arguments(uses Point class)
+        Creates a Line
+
+        Args:
+            start_point (Point): point from where line starts
+            end_point (Point): point from where line ends
+        """
         self.start_point = start_point
         self.end_point = end_point
         
@@ -27,4 +38,14 @@ class Line:
         
         self._end_point = value
         
+    def length(self):
+        """
+        Calculates the lenght of line
+        
+        Returns:
+            float
+        """
+        return self.start_point.distance(self.end_point)
     
+    def __str__(self):
+        return f"Line from {self.start_point} to {self.end_point}"
