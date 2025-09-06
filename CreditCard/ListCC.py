6ad7f3dbdf2c1bc82f4d03c4072f78fe6f3b7873
Shahaam-Sid <muhammadgc821@gmail.com>
2025-09-06 11:55:29 +0500

    Added index(), insert() and pop()

--- CreditCard/ListCC.py
+++ CreditCard/ListCC.py
@@ -44,6 +44,62 @@ class CustomList:
         
         for i in collection:
             self.add(i)
+            
+    def index(self, id):
+        if not isinstance(id, str):
+            raise TypeError("Account ID must be a string")
+        
+        id = id.strip()
+        
+        if id is None or len(id) == 0:
+            raise ValueError("Account ID must contain Numbers") 
+                
+        if id.isspace():
+            raise ValueError("Account ID cannot be whitespaces")        
+        
+        if any(not x.isdecimal() for x in id.split()):
+            raise ValueError("Account ID must only contain Numbers")
+        
+        for i in range(len(self._list)):
+            if self._list[i].account_id == id:
+                return i
+            
+        raise ValueError("ID Not Matched | Credit Card Not Found")
+                
+    def insert(self, index, credit_card):
+        if not isinstance(index, int):
+            raise TypeError("Index must be an Integer Value")
+        if not isinstance(credit_card, CreditCard):
+            raise TypeError("Can only insert Credit Card object")
+        
+        n = len(self._list)
+        
+        if index < 0:
+            index = n + index
+        
+        new_list = [None] * (n + 1)
+        
+        for i in range(n + 1):
+            if i < index:
+                new_list[i] = self._list[i]
+            if i == index:
+                new_list[i] = credit_card
+            if i > index:
+                new_list[i] = self._list[i - 1]
+                
+        self._list = new_list
+        
+    def pop(self, index):
+        if not isinstance(index, int):
+            raise ValueError("Index must be a Integer Value")
+        
+        credit_card = self._list[index]
+        
+        del self[index]
+        
+        return credit_card
+                
+        
         
         
                                    
