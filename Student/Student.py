1a0f0d6adb05b2d0d939bcd0532a44eb40bbd8b4
Shahaam-Sid <muhammadgc821@gmail.com>
2025-09-07 11:52:53 +0500

    Added Students Class, Initialized it, and added Property and Setters

---
+++ Student/Student.py
@@ -0,0 +1,74 @@
+class Students:
+    def __init__(self, name, field, sem, s_no):
+        self.name = name
+        self.field = field
+        self.sem = sem
+        self.s_no = s_no
+        
+    @property
+    def name(self):
+        return self._name 
+    
+    @property
+    def field(self):
+        return self._field
+    
+    @property
+    def sem(self):
+        return self._sem
+    
+    @property
+    def s_no(self):
+        return self._s_no
+    
+    @name.setter
+    def name(self, value):
+        value = value.strip()
+        if not isinstance(value, str):
+            raise TypeError('Name must be a String')
+        if len(value) == 0:
+            raise ValueError('Name cannot be empty')
+        if any(not x.isalpha() for x in value.split()):
+            raise ValueError("Name can only contain Alphabets")
+
+        self._name = value
+        
+    @field.setter
+    def field(self, value):
+        if not isinstance(value, str):
+            raise TypeError('Field must be a String')
+        if value not in ['CS', 'SE', 'AI', 'DS']:
+            raise ValueError("Invalid Field | Valid: ['CS', 'SE', 'AI', 'DS']")
+        
+        self._field = value
+        
+    @sem.setter
+    def sem(self, value):
+        if not isinstance(value, int):
+            raise TypeError('Semester Must be a Integer Value')
+        if value not in range(1, 9):
+            raise ValueError("Thier exist only 8 Semesters")
+        
+        self._sem = value
+        
+    @s_no.setter
+    def s_no(self, value):
+        if not isinstance(value, str):
+            raise TypeError('Serial Number must be a String')
+        if not value.isdecimal:
+            raise ValueError('Serial Number must obly contain Integers')
+        if not len(value) == 7:
+            raise ValueError('Serial Number must contain 7 digits')
+        
+        self._s_no = value
+    
+    def __str__(self):
+        return f"Name: {self.name}, Field: {self.field}, Semester: {self.sem}, Serial No.: {self.s_no}"
+    
+    
+
+std1 = Students('Muhammad Shahaam Siddiqui', 'CS', 2, '1234567')
+ 
+std1.s_no = '00000070'
+
+print(std1)
